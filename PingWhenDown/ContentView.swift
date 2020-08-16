b7148e3636d921c2b26712d062a675c0e74ad0c7
João de Vasconcelos <johnyvasconcelos@icloud.com>
2020-08-16 21:46:26 +0100

    Initial Commit

---
+++ PingWhenDown/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PingWhenDown
+//
+//  Created by João de Vasconcelos on 16/08/2020.
+//  Copyright © 2020 João de Vasconcelos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
